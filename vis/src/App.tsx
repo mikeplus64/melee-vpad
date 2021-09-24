14985d2a6f2fac5f01d2456c1fd40f9d1d583d0b
Mike Ledger <eleventynine@gmail.com>
2021-09-24 15:26:57 +1000

    vis improvements

--- vis/src/App.tsx
+++ vis/src/App.tsx
@@ -1,79 +1,119 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const w = 250;
-const w2 = 250 / 2;
+const w = 400;
+const strokeWidth = 3 / 400;
 
+function animate(fps = 60, fn: (delta: number) => boolean) {
+  const interval = 1000.0 / fps;
+  const tolerance = 0.1;
+  let alive = true;
+  function start() {
+    let then = performance.now();
+    function animateLoop(now: DOMHighResTimeStamp) {
+      if (alive) requestAnimationFrame(animateLoop);
+      const delta = now - then;
+      if (delta >= interval - tolerance) {
+        then = now - (delta % interval);
+        alive = fn(delta);
+      }
+    }
+    requestAnimationFrame(animateLoop);
+  }
+  start();
+}
+
+function d(hist: { x: number; y: number }[]) {
+  let s = `M ${hist[0].x},${hist[0].y} L`;
+  for (const { x, y } of hist) {
+    s += ` ${x},${y}`;
+  }
+  return s;
+}
+
+const initialCursor = { x: 0, y: 0 };
+const initialHist = [
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+];
+
+const initialState = {
+  cursor: initialCursor,
+  hist: initialHist,
+};
+
+let globalx, globaly;
 function Dot() {
-  const [state, setState] = useState({ x: 0, y: 0 });
+  const [, setRender] = useState(false);
+  const state = useRef(initialState);
 
   useEffect(() => {
     let alive = true;
+    console.log("initialising dot");
     window.addEventListener("gamepadconnected", (ev) => {
-      console.log("Got gamepad", ev.gamepad);
+      console.log("gamepadconnected");
       const gamepad = ev.gamepad;
-      function loop() {
+      animate(15, () => {
         const [x, y] = gamepad.axes;
-        console.log(gamepad.axes);
-        setState({ x: x * w2 + w2, y: y * w2 + w2 });
-        if (alive) requestAnimationFrame(loop);
-      }
-      loop();
+        globalx = x;
+        globaly = x;
+        if (state.current.cursor.x !== x || state.current.cursor.y !== y) {
+          state.current.cursor.x = x;
+          state.current.cursor.y = y;
+          const [del] = state.current.hist.splice(0, 1);
+          del.x = x;
+          del.y = y;
+          state.current.hist.push(del);
+          setRender((r) => !r);
+        }
+        return alive;
+      });
     });
     return () => {
       alive = false;
     };
   }, []);
 
-  return <circle cx={state.x} cy={state.y} r={4} fill="red" />;
+  return (
+    <>
+      <circle
+        cx={state.current.cursor.x}
+        cy={state.current.cursor.y}
+        r={8 / w}
+        fill="red"
+      />
+      <path
+        fill="none"
+        stroke="rgba(0,0,0,0.5)"
+        strokeWidth={strokeWidth}
+        d={d(state.current.hist)}
+      />
+      <path
+        fill="none"
+        stroke="black"
+        strokeWidth={strokeWidth * 2}
+        d={`M 0,0 L ${state.current.cursor.x},${state.current.cursor.y}`}
+      />
+    </>
+  );
 }
 
 function App() {
   return (
     <div style={{ width: w + "px", height: w + "px" }}>
-      <svg viewBox={`-4 -4 ${w + 8} ${w + 8}`}>
-        <rect
-          x={0}
-          y={0}
-          width={w2}
-          height={w2}
-          fill="rgba(0,0,0,0.25)"
-          stroke="black"
-          strokeWidth={1}
-        />
-        <rect
-          x={w2}
-          y={0}
-          width={w2}
-          height={w2}
-          fill="rgba(0,0,0,0.25)"
-          stroke="black"
-          strokeWidth={1}
-        />
-        <rect
-          x={0}
-          y={w2}
-          width={w2}
-          height={w2}
-          fill="rgba(0,0,0,0.25)"
-          stroke="black"
-          strokeWidth={1}
-        />
-        <rect
-          x={w2}
-          y={w2}
-          width={w2}
-          height={w2}
-          fill="rgba(0,0,0,0.25)"
-          stroke="black"
-          strokeWidth={1}
-        />
+      <svg viewBox={`-2 -2 4 4`}>
+        <rect x={-1} y={-1} width={2} height={1} fill="rgba(255,0,255,0.25)" />
+        <rect x={0} y={-1} width={1} height={1} fill="rgba(255,0,0,0.25)" />
+        <rect x={-1} y={0} width={1} height={1} fill="rgba(0,255,255,0.25)" />
+        <rect x={0} y={0} width={1} height={1} fill="rgba(0,0,255,0.25)" />
         <circle
-          cx={w2}
-          cy={w2}
-          r={w2}
-          fill="rgba(0,0,0,0.5)"
+          cx={0}
+          cy={0}
+          r={1}
           stroke="black"
-          strokeWidth={1}
+          strokeWidth={strokeWidth}
+          fill="none"
         />
         <Dot />
       </svg>

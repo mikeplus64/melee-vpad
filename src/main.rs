dbcf151d2ee7407626701784087231353dcb4e90
Mike Ledger <eleventynine@gmail.com>
2021-09-29 02:50:15 +1000

    configuration and premature optimisation

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,120 @@
 use crossbeam::queue::SegQueue;
 use env_logger;
 use evdev;
+use evdev::Key;
 use log;
+use modular_bitfield::{
+    bitfield,
+    specifiers::{B3, B6},
+};
 use std::sync::Arc;
 use std::thread;
 use std::time::{Duration, Instant};
 use uinput;
 use uinput::event::absolute::Position as AbsolutePosition;
-use uinput::event::controller::{Controller as C, DPad, GamePad as GP};
+use uinput::event::controller::{DPad, GamePad as GP};
 
 const KEYBOARD_NAME: &str = "CATEX TECH. 84EC-XRGB";
 
-const J_MOD1: f32 = 64.0 / 128.0; // c stick only
-const J_MOD2: f32 = 48.0 / 128.0;
-const J_MOD12: f32 = 32.0 / 128.0; // c stick only
+// =================
+// Key Bindings
+// =================
+// GCC
+struct DPadBinds {
+    up: Key,
+    down: Key,
+    left: Key,
+    right: Key,
+}
 
+const CONTROL_STICK: DPadBinds = DPadBinds {
+    up: Key::KEY_W,
+    down: Key::KEY_S,
+    left: Key::KEY_A,
+    right: Key::KEY_D,
+};
+
+const C_STICK: DPadBinds = DPadBinds {
+    up: Key::KEY_K,
+    down: Key::KEY_COMMA,
+    left: Key::KEY_M,
+    right: Key::KEY_DOT,
+};
+
+const DPAD: DPadBinds = DPadBinds {
+    up: Key::KEY_UP,
+    down: Key::KEY_DOWN,
+    left: Key::KEY_LEFT,
+    right: Key::KEY_RIGHT,
+};
+
+const BTN_A: Key = Key::KEY_L;
+const BTN_B: Key = Key::KEY_SEMICOLON;
+const BTN_Z: Key = Key::KEY_APOSTROPHE;
+const BTN_X: Key = Key::KEY_SPACE;
+const BTN_Y: Key = Key::KEY_LEFTALT; // I don't really use this at all
+const BTN_START: Key = Key::KEY_T;
+const BTN_L: Key = Key::KEY_P;
+const BTN_R: Key = Key::KEY_LEFTBRACE;
+// Modifiers
+const BTN_MOD1: Key = Key::KEY_LEFTSHIFT;
+const BTN_MOD2: Key = Key::KEY_ENTER;
+// =================
+
+const J_MOD1_INCR: f32 = 0.3875;
+const J_MOD1_AROUND_Y: f32 = 0.31;
+const J_MOD2: f32 = 48.0 / 128.0;
 const TRIGGER_MOD1: f32 = 129.0 / 256.0;
 
-const RATE_TARGET: Duration = Duration::from_micros(500);
+const RATE_TARGET: Duration = Duration::from_micros(250);
 
-#[derive(Copy, Clone, Default, Debug)]
-struct JoyState {
-    // control stick
-    control_stick_up: bool,
-    control_stick_down: bool,
-    control_stick_left: bool,
-    control_stick_right: bool,
-    control_stick_active: bool,
-    control_stick_x: f32,
-    control_stick_y: f32,
-    control_stick_mul: f32,
-    control_stick_update: bool,
+#[bitfield]
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
+struct DPadState {
+    up: bool,
+    down: bool,
+    left: bool,
+    right: bool,
+    updated: bool,
+    #[skip]
+    __: B3,
+}
 
-    // c stick
-    c_up: bool,
-    c_down: bool,
-    c_left: bool,
-    c_right: bool,
-    c_x: f32,
-    c_y: f32,
+impl DPadState {
+    pub fn is_active(&self) -> bool {
+        self.left() || self.right() || self.up() || self.down()
+    }
 
-    // triggers
-    l_trigger: bool,
-    r_trigger: bool,
-    l_trigger_depth: f32,
-    c_trigger: bool,
+    pub fn update(&mut self, binds: DPadBinds, key: Key, value: bool) -> bool {
+        if key == binds.up {
+            let prev = self.up();
+            self.set_up(value);
+            self.set_updated(prev != value);
+            true
+        } else if key == binds.down {
+            let prev = self.down();
+            self.set_down(value);
+            self.set_updated(prev != value);
+            true
+        } else if key == binds.left {
+            let prev = self.left();
+            self.set_left(value);
+            self.set_updated(prev != value);
+            true
+        } else if key == binds.right {
+            let prev = self.right();
+            self.set_right(value);
+            self.set_updated(prev != value);
+            true
+        } else {
+            false
+        }
+    }
+}
 
+#[bitfield]
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
+struct JoyButtons {
     // buttons
     a: bool,
     b: bool,
@@ -53,166 +122,137 @@ struct JoyState {
     y: bool,
     z: bool,
     start: bool,
+    // digital triggers
+    l: bool,
+    r: bool,
+}
 
-    // dpad
-    dpad_up: bool,
-    dpad_down: bool,
-    dpad_left: bool,
-    dpad_right: bool,
-
-    // modifiers
+#[bitfield]
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
+struct Modifiers {
     mod1: bool,
     mod2: bool,
+    #[skip]
+    __: B6,
+}
 
-    // tag
-    tag: u32,
+#[derive(Copy, Clone, Default, Debug)]
+struct JoyState {
+    // control stick
+    control_stick: DPadState,
+    control_stick_x: f32,
+    control_stick_y: f32,
+    // c stick
+    c_stick: DPadState,
+    c_stick_x: f32,
+    c_stick_y: f32,
+    // dpad
+    dpad: DPadState,
+    // analog l trigger
+    l_trigger: f32,
+    // digital buttons
+    btn: JoyButtons,
+    // modifiers
+    m: Modifiers,
 }
 
 impl JoyState {
     fn update_flags(&mut self, event: evdev::InputEvent) {
-        use evdev::{InputEventKind, Key};
-        if let InputEventKind::Key(k) = event.kind() {
-            if let Some(r) = match k {
-                // wasd -> control stick
-                Key::KEY_W => Some(&mut self.control_stick_up),
-                Key::KEY_S => Some(&mut self.control_stick_down),
-                Key::KEY_A => Some(&mut self.control_stick_left),
-                Key::KEY_D => Some(&mut self.control_stick_right),
-                Key::KEY_LEFTSHIFT => Some(&mut self.mod1),
-                Key::KEY_ENTER => Some(&mut self.mod2),
-                _ => None,
-            } {
-                let cur = *r;
-                let next = event.value() != 0;
-                self.control_stick_update = self.control_stick_update || cur != next;
-                *r = event.value() != 0;
-            } else if let Some(r) = match k {
-                // hbnm -> C stick
-                // Key::KEY_H => Some(&mut self.c_up),
-                // Key::KEY_N => Some(&mut self.c_down),
-                // Key::KEY_B => Some(&mut self.c_left),
-                // Key::KEY_M => Some(&mut self.c_right),
-                // jkl -> ABZ buttons
-                // Key::KEY_J => Some(&mut self.a),
-                // Key::KEY_K => Some(&mut self.b),
-                // Key::KEY_L => Some(&mut self.z),
-                // io -> triggers
-                // Key::KEY_I => Some(&mut self.l_trigger),
-                // Key::KEY_O => Some(&mut self.r_trigger),
-
-                // hbnm -> C stick
-                Key::KEY_K => Some(&mut self.c_up),
-                Key::KEY_COMMA => Some(&mut self.c_down),
-                Key::KEY_M => Some(&mut self.c_left),
-                Key::KEY_DOT => Some(&mut self.c_right),
-
-                // p spc -> ABZX buttons
-                Key::KEY_L => Some(&mut self.a),
-                Key::KEY_SEMICOLON => Some(&mut self.b),
-                Key::KEY_APOSTROPHE => Some(&mut self.z),
-                Key::KEY_SPACE => Some(&mut self.x),
-
-                // -= -> triggers
-                Key::KEY_P => Some(&mut self.l_trigger),
-                Key::KEY_LEFTBRACE => Some(&mut self.r_trigger),
-
-                Key::KEY_T => Some(&mut self.start),
-                // dpad
-                Key::KEY_UP => Some(&mut self.dpad_up),
-                Key::KEY_DOWN => Some(&mut self.dpad_down),
-                Key::KEY_LEFT => Some(&mut self.dpad_left),
-                Key::KEY_RIGHT => Some(&mut self.dpad_right),
-                // modifiers
-                Key::KEY_LEFTSHIFT => Some(&mut self.mod1),
-                _ => None,
-            } {
-                *r = event.value() != 0;
+        self.control_stick.set_updated(false);
+        self.c_stick.set_updated(false);
+        self.dpad.set_updated(false);
+        if let evdev::InputEventKind::Key(k) = event.kind() {
+            let value = event.value();
+            if value > 1 {
+                // skip a repeat key event entirely, we would have already
+                // processed it being pressed anyway
+                return;
+            }
+            let value = value != 0;
+            if self.control_stick.update(CONTROL_STICK, k, value) {
+            } else if self.c_stick.update(C_STICK, k, value) {
+            } else if self.dpad.update(DPAD, k, value) {
+            } else {
+                match k {
+                    _ if k == BTN_A => self.btn.set_a(value),
+                    _ if k == BTN_B => self.btn.set_b(value),
+                    _ if k == BTN_X => self.btn.set_x(value),
+                    _ if k == BTN_Y => self.btn.set_y(value),
+                    _ if k == BTN_Z => self.btn.set_z(value),
+                    _ if k == BTN_START => self.btn.set_start(value),
+                    _ if k == BTN_L => self.btn.set_l(value),
+                    _ if k == BTN_R => self.btn.set_r(value),
+                    _ if k == BTN_MOD1 => self.m.set_mod1(value),
+                    _ if k == BTN_MOD2 => self.m.set_mod2(value),
+                    _ => {}
+                }
             }
         }
-        self.tag += 1;
     }
 
-    fn update_analog(&mut self) {
-        if self.control_stick_update {
-            let active0 = self.control_stick_active;
+    fn update_analog(&mut self, prev: &Self) {
+        let mod1 = self.m.mod1();
+        let mod2 = self.m.mod2();
+        let mod_changed = self.m != prev.m;
+        let j_mul = if mod2 { J_MOD2 } else { 1.0 };
 
-            self.control_stick_mul = if self.mod2 { J_MOD2 } else { 1.0 };
-            self.control_stick_active = self.control_stick_up
-                || self.control_stick_down
-                || self.control_stick_left
-                || self.control_stick_right;
-
-            let vx = (self.control_stick_right as i8) - (self.control_stick_left as i8);
-            let vy = (self.control_stick_down as i8) - (self.control_stick_up as i8);
-            let mut x = vx as f32;
-            let mut y = vy as f32;
-            if self.mod1 && self.control_stick_active {
-                let x0 = self.control_stick_x;
-                let y0 = self.control_stick_y;
-                if !active0 {
-                    if vx.abs() == 1 && vy.abs() == 1 {
-                        y = 0.31 * vy as f32;
-                    }
-                } else {
-                    if !(y0.abs() < 0.01) {
-                        x = x0 + 0.3875 * vx as f32;
-                    }
-                    if !(x0.abs() < 0.01) {
-                        y = y0 + 0.31 * vy as f32;
+        // update control stick
+        if self.control_stick != prev.control_stick || mod_changed {
+            let active = self.control_stick.is_active();
+            if active {
+                let vx = (self.control_stick.right() as i8) - (self.control_stick.left() as i8);
+                let vy = (self.control_stick.down() as i8) - (self.control_stick.up() as i8);
+                let (mut x, mut y) = (vx as f32, vy as f32);
+                if mod1 {
+                    let active0 = prev.control_stick.is_active();
+                    let (x0, y0) = (self.control_stick_x, self.control_stick_y);
+                    let (vhoriz, vvert) = (vx.abs() == 1, vy.abs() == 1);
+                    if !active0 {
+                        if vhoriz && vvert {
+                            y *= J_MOD1_AROUND_Y;
+                        }
+                    } else {
+                        if y0.abs() > 0.01 {
+                            x = x0 + J_MOD1_INCR * vx as f32;
+                        }
+                        if x0.abs() > 0.01 {
+                            y = y0 + J_MOD1_AROUND_Y * vy as f32;
+                        }
                     }
                 }
-                if !self.mod2 && x.abs() < 0.99 && y.abs() < 0.99 {
+                if mod2 && x.abs() < 0.99 && y.abs() < 0.99 {
                     if x.abs() > y.abs() {
                         x = vx as f32;
                     } else {
                         y = vy as f32;
                     }
                 }
+                self.control_stick_x = (x * j_mul).clamp(-1.0, 1.0);
+                self.control_stick_y = (y * j_mul).clamp(-1.0, 1.0);
+            } else {
+                self.control_stick_x = 0.0;
+                self.control_stick_y = 0.0;
             }
-            self.control_stick_x = (x * self.control_stick_mul).clamp(-1.0, 1.0);
-            self.control_stick_y = (y * self.control_stick_mul).clamp(-1.0, 1.0);
         }
-        self.c_x = self.c_joyval(self.c_right, self.c_left);
-        self.c_y = self.c_joyval(self.c_down, self.c_up);
-        self.l_trigger_depth = self.triggerval();
-    }
 
-    // only used for C stick
-    fn c_joy_modval(&self) -> f32 {
-        if self.mod1 && self.mod2 {
-            J_MOD12
-        } else if self.mod1 {
-            J_MOD1
-        } else if self.mod2 {
-            J_MOD2
-        } else {
-            1.0
+        // update C stick
+        if self.c_stick != prev.c_stick || mod2 != prev.m.mod2() {
+            self.c_stick_x = ((self.c_stick.right() as i8) - (self.c_stick.left() as i8)) as f32;
+            self.c_stick_y = ((self.c_stick.down() as i8) - (self.c_stick.up() as i8)) as f32;
+            self.c_stick_x *= j_mul;
+            self.c_stick_y *= j_mul;
         }
-    }
 
-    fn c_joyval(&self, high: bool, low: bool) -> f32 {
-        if high && low {
-            0.0
-        } else if high {
-            1.0 * self.c_joy_modval()
-        } else if low {
-            -1.0 * self.c_joy_modval()
-        } else {
-            0.0
-        }
-    }
-
-    fn triggerval(&self) -> f32 {
-        // light shield and full shield and that's it
-        if self.l_trigger {
-            if self.mod1 {
+        // update L trigger (R trigger is just digital here)
+        self.l_trigger = if self.btn.l() {
+            if mod1 {
                 TRIGGER_MOD1
             } else {
                 1.0
             }
         } else {
             0.0
-        }
+        };
     }
 }
 
@@ -260,17 +300,20 @@ fn main() -> uinput::Result<()> {
         let q = q1;
         let mut kbd = get_keyboard();
         let mut state = JoyState::default();
+        let mut prev = state;
         loop {
             let mut any_events = false;
-            state.control_stick_update = false;
-            for ev in kbd.fetch_events().unwrap() {
+            let events = kbd.fetch_events().unwrap();
+            for ev in events {
                 any_events = true;
                 state.update_flags(ev);
             }
             if any_events {
-                state.update_analog();
+                state.update_analog(&prev);
                 q.push(state);
             }
+            prev = state;
+            // log::debug!("input loop took {:?}", now.elapsed());
         }
     });
 
@@ -293,37 +336,49 @@ fn main() -> uinput::Result<()> {
                 state.control_stick_y,
             )?;
 
-            // c stick
-            update_joy(&mut vjoy, &AbsolutePosition::RX, prev.c_x, state.c_x)?;
-            update_joy(&mut vjoy, &AbsolutePosition::RY, prev.c_y, state.c_y)?;
+            // R trigger, put it here to marginally reduce latency on wavedash
+            // which is the only 1 frame input I can think of at the moment
+            update_btn(&mut vjoy, prev.btn.r(), state.btn.r(), &GP::TR)?;
 
-            // trigger
-            if prev.l_trigger_depth != state.l_trigger_depth {
+            // L trigger
+            if prev.l_trigger != state.l_trigger {
                 vjoy.position(
                     &AbsolutePosition::Z,
                     // this doesn't make any sense to me but it works in dolphin
-                    (127.0 + state.l_trigger_depth * 128.0) as i32,
+                    (127.0 + state.l_trigger * 128.0) as i32,
                 )?;
             }
 
             // buttons
-            for (prev, cur, ev) in &[
-                (prev.a, state.a, C::GamePad(GP::East)),
-                (prev.b, state.b, C::GamePad(GP::South)),
-                (prev.x, state.x, C::GamePad(GP::North)),
-                (prev.z, state.z, C::GamePad(GP::West)),
-                (prev.start, state.start, C::GamePad(GP::Start)),
-                (prev.dpad_up, state.dpad_up, C::DPad(DPad::Up)),
-                (prev.dpad_down, state.dpad_down, C::DPad(DPad::Down)),
-                (prev.dpad_left, state.dpad_left, C::DPad(DPad::Left)),
-                (prev.dpad_right, state.dpad_right, C::DPad(DPad::Right)),
-                (prev.r_trigger, state.r_trigger, C::GamePad(GP::TR)),
-            ] {
-                if *prev && !*cur {
-                    vjoy.release(ev)?;
-                } else if !*prev && *cur {
-                    vjoy.press(ev)?;
-                }
+            if prev.btn != state.btn {
+                update_btn(&mut vjoy, prev.btn.a(), state.btn.a(), &GP::East)?;
+                update_btn(&mut vjoy, prev.btn.b(), state.btn.b(), &GP::South)?;
+                update_btn(&mut vjoy, prev.btn.x(), state.btn.x(), &GP::North)?;
+                update_btn(&mut vjoy, prev.btn.y(), state.btn.y(), &GP::TL)?;
+                update_btn(&mut vjoy, prev.btn.z(), state.btn.z(), &GP::West)?;
+                update_btn(&mut vjoy, prev.btn.start(), state.btn.start(), &GP::Start)?;
+            }
+
+            // c stick
+            update_joy(
+                &mut vjoy,
+                &AbsolutePosition::RX,
+                prev.c_stick_x,
+                state.c_stick_x,
+            )?;
+            update_joy(
+                &mut vjoy,
+                &AbsolutePosition::RY,
+                prev.c_stick_y,
+                state.c_stick_y,
+            )?;
+
+            // dpad
+            if state.dpad.updated() {
+                update_btn(&mut vjoy, prev.dpad.up(), state.dpad.up(), &DPad::Up)?;
+                update_btn(&mut vjoy, prev.dpad.down(), state.dpad.down(), &DPad::Up)?;
+                update_btn(&mut vjoy, prev.dpad.left(), state.dpad.left(), &DPad::Up)?;
+                update_btn(&mut vjoy, prev.dpad.right(), state.dpad.right(), &DPad::Up)?;
             }
 
             vjoy.synchronize()?;
@@ -349,6 +404,21 @@ fn get_keyboard() -> evdev::Device {
 
 fn jval(value: f32) -> i32 {
     (value * 127.0) as i32
+}
+
+fn update_btn<T: uinput::event::Press + uinput::event::Release>(
+    device: &mut uinput::Device,
+    prev: bool,
+    cur: bool,
+    ev: &T,
+) -> uinput::Result<()> {
+    if prev && !cur {
+        device.release(ev)
+    } else if !prev && cur {
+        device.press(ev)
+    } else {
+        Ok(())
+    }
 }
 
 fn update_joy<T: uinput::event::Position>(
